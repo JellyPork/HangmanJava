131c4bfd2b958d91b850a0c312d5a5087f67c78c
Arulich <arulichit@gmail.com>
2021-04-18 21:27:03 -0700

    first commit

---
+++ src/main/java/ControlJuego.java
@@ -0,0 +1,100 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ControlJuego {
+    
+    
+    
+    public static String generarPalabra(){
+        String[] listaPalabras = {"Javier","Javier","pescado","Valeria","Probando","Universidad","Jose"};
+        String palabra;
+        
+        palabra = listaPalabras[(int)(Math.random()*listaPalabras.length)];
+       
+        
+        return palabra;
+        
+    }
+    
+    public static String[] checarLetrasAcertadas (String jugada, String palabraInicial, String []palabraJugador2){
+            //ejecuta la jugada y devuelve la palabra modificada si ha acertado alguna letra
+
+            for (int i = 0; i < palabraInicial.length(); i++) {
+                if (palabraInicial.charAt(i) == jugada.charAt(0)) {
+                    palabraJugador2[i] = jugada;
+                }
+            }
+            return palabraJugador2;
+    }
+    
+    public static int contarCaracteres (String cadena,char caracter){
+            int posicion, contador = 0;
+            //se busca la primera vez que aparece
+            posicion = cadena.indexOf(caracter);
+            while (posicion != -1) { //mientras se encuentre el caracter
+                contador++;           //se cuenta
+                //se sigue buscando a partir de la posición siguiente a la encontrada
+                posicion = cadena.indexOf(caracter, posicion + 1);
+            }
+            return contador;
+    }    
+    
+    public static void dibujarAhorcado(int intentoFallido){
+        switch(intentoFallido){
+            case 1: System.out.println("    ______");
+                    System.out.println("   |     |");
+                    System.out.println("   |     O"  );
+                    System.out.println("   |        ");
+                    System.out.println("   |        ");
+                    System.out.println("___|________");
+                    break;
+            case 2: System.out.println("    ______");
+                    System.out.println("   |     |");
+                    System.out.println("   |     O");
+                    System.out.println("   |     | ");
+                    System.out.println("   |      ");
+                    System.out.println("___|________");
+                    break;  
+            case 3: System.out.println("    ______");
+                    System.out.println("   |     |");
+                    System.out.println("   |     O");
+                    System.out.println("   |    /| ");
+                    System.out.println("   |        ");
+                    System.out.println("___|________");
+                    break;
+            case 4: System.out.println("    ______");
+                    System.out.println("   |     |");
+                    System.out.println("   |     O");
+                    System.out.println("   |    /|/ ");
+                    System.out.println("   |        ");
+                    System.out.println("___|________");
+                    break;
+            case 5: System.out.println("    ______");
+                    System.out.println("   |     |");
+                    System.out.println("   |     O");
+                    System.out.println("   |    /|/ ");
+                    System.out.println("   |    /  ");
+                    System.out.println("___|________");
+                    break;
+            case 6: System.out.println("    ______");
+                    System.out.println("   |     |");
+                    System.out.println("   |     O");
+                    System.out.println("   |    /|/ ");
+                    System.out.println("   |    / /");
+                    System.out.println("___|________");
+                    break;
+        }
+
+    }
+}
+
---
+++ src/main/java/Juego.java
@@ -0,0 +1,115 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Juego {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String jugarOtra="si"; //para jugar otra partida
+        while(jugarOtra.equals("si")) {
+            int puntuacion = 0;
+
+            System.out.println("Se esta generando la palabra a adivinar");
+            String palabraInicial = ControlJuego.generarPalabra();
+            String palabraAux = palabraInicial;
+            palabraInicial = "";
+
+            for (int l = 0; l < palabraAux.length(); l++) {
+                palabraInicial += "" + palabraAux.charAt(l) + " ";
+            }
+
+
+            String palabraJuego = ""; // la palabra que va a ir rellenando el jugador 2
+            System.out.println("La palabra a buscar tiene la siguiente longitud: ");
+
+            for (int i = 0; i < palabraInicial.length() / 2; i++) { //imprimo _ por cada letra que tenga la palabra
+                palabraJuego += "_ ";
+            }
+
+            System.out.println(palabraJuego);
+
+            String jugada;
+            String[] palabraJugador = new String[palabraInicial.length()];
+            for (int m = 0; m < palabraJuego.length(); m++) {
+                palabraJugador[m] = "" + palabraJuego.charAt(m);
+            }
+            int caracteresAnterior = palabraInicial.length() / 2; //si no varia el numero de '_' en cada tirada significa
+            //que no ha introducido un caracter valido y pierde una vida
+            int caracteresActual;
+
+
+            while (puntuacion < 5) { //si llega a perder 5 vidas el jugador pierde
+
+                System.out.println("Numero de vidas restantes: " + (puntuacion - 5) + " de 5.");
+
+                System.out.println("Introduzca una letra: ");
+                jugada = teclado.nextLine();
+
+                while (jugada.length() != 1) { //Validacion de la entrada del jugador 2
+                    System.out.println("Introduzca SOLO una letra: ");
+                    jugada = teclado.nextLine();
+                }
+
+                palabraJugador = ControlJuego.checarLetrasAcertadas(jugada, palabraInicial, palabraJugador); //comprueba si la letra esta en la palabra
+
+                for (int m = 0; m < palabraJugador.length; m++) { //imprimo lo que ve el usuario
+                    System.out.print(palabraJugador[m]);
+                }
+                System.out.println();
+
+                palabraJuego = "";
+
+                for (int x = 0; x < palabraInicial.length(); x++) {
+                    palabraJuego += palabraJugador[x];
+                }
+
+                caracteresActual = ControlJuego.contarCaracteres(palabraJuego, '_');
+                if (caracteresActual == 0) {
+                    puntuacion = 6; //si puntuacion vale 6 es que el usuario ha ganado la partida
+                }
+
+                if (caracteresActual == caracteresAnterior) { // compruebo si las '_' han variado en el turno para que pierda una vida
+                    puntuacion++;
+                }
+                if (puntuacion == 5) {
+                    System.out.println();
+                    System.out.println();
+                    System.out.println();
+                    System.out.println("Su cuello no ha aguantado más y se ha partido");
+                }
+                if (puntuacion == 6) {
+                    System.out.println();
+                    System.out.println();
+                    System.out.println();
+                    System.out.println("Se ha librado por los pelos!!!!!!!!!");
+                }
+                ControlJuego.dibujarAhorcado(puntuacion);
+                caracteresAnterior = caracteresActual;
+            }
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("Juego finalizado");
+
+            System.out.println("¿Desea jugar otra partida? Introduzca si para continuar");
+            jugarOtra = (teclado.nextLine()).toLowerCase();
+        }
+    }
+    
+    
+
+    
+}
